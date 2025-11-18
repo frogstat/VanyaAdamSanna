2b7060cd874b295e9650668bcddd56b19e708330
Adam <adamgareb@hotmail.com>
2025-11-18 14:16:09 +0100

    Added javadoc in Player.java

--- Player.java
+++ Player.java
@@ -1,28 +1,60 @@
+/**
+ * A player class for the user to have their unique name and track their score.
+ * -Adam
+ */
 public class Player {
     private String name;
     private int score;
 
+    /**
+     * The constructor for the player. Player starts with 0 points
+     * @param name To let the player put in it's name
+     * -Adam
+     */
     public Player(String name) {
         this.name = name;
         this.score = 0;
     }
 
+    /**
+     * A getter for getting player name
+     * @return Returns the name of player
+     * -Adam
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * A getter for getting score
+     * @return Returns the score of player
+     * -Adam
+     */
     public int getScore(){
         return score;
     }
 
+    /**
+     * Method to add score for the player
+     * -Adam
+     */
     public void addScore(int score){
         this.score += score;
     }
 
+    /**
+     * A reset score method, to reset code
+     * -Adam
+     */
     public void resetScore(){
         this.score = 0;
     }
 
+    /**
+     * A toString method
+     * @return Returns the player's name and shows it's current points
+     * -Adam
+     */
     public String toString(){
         return "Player: " + getName() + " has " + getScore() + " points";
     }
