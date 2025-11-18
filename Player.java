4de5f964694bb45318f38c95c230523cd982ba17
Adam <adamgareb@hotmail.com>
2025-11-18 13:53:09 +0100

    Completed Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,29 @@
 public class Player {
+    private String name;
+    private int score;
 
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void addScore(int score){
+        this.score += score;
+    }
+
+    public void resetScore(){
+        this.score = 0;
+    }
+
+    public String toString(){
+        return "Player: " + getName() + " has " + getScore() + " points";
+    }
 }
