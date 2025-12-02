201e2a4a50204ed61fbb499f0c7a4c41f1b49723
frogstat <frogstatftw@gmail.com>
2025-12-02 15:07:49 +0100

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-java -jar target/VAS_Maven-1.0-SNAPSHOT.jar
+if [ -e target/VAS_Maven-1.0-SNAPSHOT.jar ]; then
+	java -jar target/VAS_Maven-1.0-SNAPSHOT.jar
+else
+	echo "There is no game file to run. Have you packaged it?"
+fi
